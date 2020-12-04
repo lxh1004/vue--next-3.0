b143c263baff3ee8e25e6ce53c71a32f5374c63f
lxhua1004 <7489568+lxhua1004@user.noreply.gitee.com>
2020-12-04 14:43:42 +0800

    vue 3.0 setup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./App.vue";
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 import router from "./router";
 import store from "./store";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(ElementPlus)
+  .mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ul>
+      <li v-for="item in stus" :key="item.id">{{ item.val }}</li>
+    </ul>
+
+    <p>{{ count }}</p>
+    <p>
+      <el-button @click="add" type="primary">添加</el-button>
+      <el-button @click="remove" type="danger">删除</el-button>
+    </p>
+
+    <ul>
+      <li v-for="item in arr" :key="item">{{ item }}</li>
+    </ul>
+    <p>biaozhu tongji peizhi</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { ref, reactive, getCurrentInstance } from 'vue'
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+  name: 'Home',
+
+  setup() {
+    const { ctx } = getCurrentInstance()
+    console.log(ctx, '替代后的this')
+    //ref函数注意点
+    //ref函数只能监听 简单类型的变化， 不能监听复杂类型的变化（对象|数组）
+    //reactive可以监听复杂类型的数据变化
+
+    const count = ref(0)
+    const arr = reactive([1, 2, 3, 4])
+    const add = () => {
+      count.value++
+    }
+
+    function remove() {
+      count.value--
+    }
+
+    let stus = reactive([
+      {
+        id: 1,
+        val: 'zs',
+      },
+      {
+        id: 2,
+        val: 'ls',
+      },
+      {
+        id: 3,
+        val: 'ww',
+      },
+    ])
+
+    return {
+      stus,
+      count,
+      add,
+      remove,
+      arr,
+    }
+  },
+}
 </script>
